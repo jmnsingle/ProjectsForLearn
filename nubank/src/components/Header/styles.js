505b6380970bca08019c581a1e64808b3b7517aa
jmnsingle <jmnogueira2011@gmail.com>
2020-01-21 12:44:47 -0300

    Main Screen created.

---
+++ nubank/src/components/Header/styles.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components/native';
+
+export const Container = styled.View`
+  align-items: center;
+  padding: 40px 0 30px;
+`;
+
+export const Top = styled.View`
+  margin-bottom: 10px;
+
+  flex-direction: row;
+  align-items: center;
+
+`;
+
+export const Logo = styled.Image`
+
+`;
+
+export const Title = styled.Text`
+  font-size: 18px;
+  color: #fff;
+  font-weight: bold;
+  margin-left: 8px;
+`;
--- nubank/src/pages/Main/index.js
+++ nubank/src/pages/Main/index.js
@@ -1,59 +1,94 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
+import { Animated } from 'react-native';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
 
-import {
-  Text, Image, StyleSheet, Dimensions, ImageBackground, StatusBar,
-} from 'react-native';
+import Header from '~/components/Header';
+import Tabs from '~/components/Tabs';
+import Menu from '~/components/Menu';
 
-const styles = StyleSheet.create({
-  container: {
-    alignItems: 'center',
-    flex: 1,
-    paddingHorizontal: 20,
-  },
-  fileName: {
-    fontWeight: 'bold',
-    marginTop: 5,
-  },
-  instructions: {
-    color: '#DDD',
-    fontSize: 14,
-    marginTop: 20,
-    textAlign: 'center',
-  },
-  logo: {
-    height: Dimensions.get('window').height * 0.11,
-    marginVertical: Dimensions.get('window').height * 0.11,
-    width: Dimensions.get('window').height * 0.11 * (1950 / 662),
-  },
-  welcome: {
-    color: '#fff',
-    fontSize: 22,
-    fontWeight: 'bold',
-    textAlign: 'center',
-  },
-});
+import { Container, Content, Card, CardHeader, CardContent, CardFooter, Title, Description, Anotation } from './styles';
 
-const Main = () => (
-  <ImageBackground
-    source={{
-      uri: 'https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/background.png',
-    }}
-    style={styles.container}
-    resizeMode="cover"
-  >
-    <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
-    <Image
-      source={{
-        uri: 'https://s3-sa-east-1.amazonaws.com/rocketseat-cdn/rocketseat_logo.png',
-      }}
-      style={styles.logo}
-      resizeMode="contain"
-    />
-    <Text style={styles.welcome}>Bem-vindo ao Template Básico!</Text>
-    <Text style={styles.instructions}>Essa é a tela principal da sua aplicação =)</Text>
-    <Text style={styles.instructions}>Você pode editar a tela no arquivo:</Text>
-    <Text style={[styles.instructions, styles.fileName]}>src/pages/Main/index.js</Text>
-  </ImageBackground>
-);
+export default function Main() {
+  let offset = 0;
+  const translateY = new Animated.Value(0);
 
-export default Main;
+  const animatedEvent = Animated.event(
+    [
+      {
+        nativeEvent: {
+          translationY: translateY,
+        }
+      }
+    ],
+    { useNativeDriver: true },
+  )
+
+  function onHandlerStateChanged(event) {
+    if(event.nativeEvent.oldState === State.ACTIVE){
+      let opened = false
+      const { translationY } = event.nativeEvent;
+
+      offset += translationY;
+
+      if(translationY >= 100) {
+        opened = true;
+      } else {
+        translateY.setValue(offset);
+        translateY.setOffset(0);
+        offset = 0;
+      }
+
+      Animated.timing(translateY, {
+        toValue: opened ? 380 : 0,
+        duration: 350,
+        useNativeDriver: true,
+      }).start(() => {
+        offset = opened ? 380 : 0;
+        translateY.setOffset(offset);
+        translateY.setValue(0);
+      });
+    }
+  }
+
+  return (
+    <Container >
+      <Header />
+
+      <Content>
+        <Menu translateY={translateY} />
+
+        <PanGestureHandler
+          onGestureEvent={animatedEvent}
+          onHandlerStateChange={onHandlerStateChanged}
+        >
+          <Card style={{
+            transform: [{
+              translateY: translateY.interpolate({
+                inputRange: [-200, 0, 400],
+                outputRange: [-50, 0, 400],
+                extrapolate: 'clamp',
+              }),
+            }],
+          }}>
+            <CardHeader>
+              <Icon name='attach-money' size={28} color="#666" />
+              <Icon name='visibility-off' size={28} color="#666" />
+            </CardHeader>
+            <CardContent>
+              <Title>Saldo Disponível</Title>
+              <Description>R$ 82.652,25</Description>
+            </CardContent>
+            <CardFooter>
+              <Anotation>
+                Transferencia de R$ 20,00 recebida de Juliano Nogueira hoje ás 06:00h
+              </Anotation>
+            </CardFooter>
+          </Card>
+        </PanGestureHandler>
+      </Content>
+
+      <Tabs translateY={translateY}/>
+    </Container>
+  );
+}
